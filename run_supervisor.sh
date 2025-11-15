0b76fc3b05b8b3db081cc3e64e57558472d9c130
im-sami <sami.nav33d@gmail.com>
2025-11-15 17:02:05 +0500

    Fixed an issue with python dependencies

--- run_supervisor.sh
+++ run_supervisor.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # run_supervisor.sh
 echo "Starting Supervisor..."
-uvicorn supervisor.main:app --host 0.0.0.0 --port 8000 --reload
+python -m uvicorn supervisor.main:app --host 0.0.0.0 --port 8000 --reload
